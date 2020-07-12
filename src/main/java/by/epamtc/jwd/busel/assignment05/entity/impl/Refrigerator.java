500c963016a58706e373ce4c6436909882b201a8
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 21:23:14 +0300

    Add almost complete bean "Refrigerator", which implements interface "Appliance"

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/impl/Refrigerator.java
@@ -0,0 +1,115 @@
+package by.epamtc.jwd.busel.assignment05.entity.impl;
+
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+
+public class Refrigerator implements Appliance {
+    private static final long serialVersionUID = -4952091167497413529L;
+    private Type type;
+    private double powerConsumption;
+    private double weight;
+    private double freezerCapacity;
+    private double overallCapacity;
+    private double height;
+    private double width;
+
+    public Refrigerator() {
+    }
+
+    public Refrigerator(Type type, double powerConsumption, double weight,
+            double freezerCapacity, double overallCapacity, double height,
+            double width) {
+        this.type = type;
+        this.powerConsumption = powerConsumption;
+        this.weight = weight;
+        this.freezerCapacity = freezerCapacity;
+        this.overallCapacity = overallCapacity;
+        this.height = height;
+        this.width = width;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public double getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(double powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getFreezerCapacity() {
+        return freezerCapacity;
+    }
+
+    public void setFreezerCapacity(double freezerCapacity) {
+        this.freezerCapacity = freezerCapacity;
+    }
+
+    public double getOverallCapacity() {
+        return overallCapacity;
+    }
+
+    public void setOverallCapacity(double overallCapacity) {
+        this.overallCapacity = overallCapacity;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return -1;
+    }
+
+    @Override
+    public String receiveTypeName() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Refrigerator{" +
+                "type=" + type +
+                ", powerConsumption=" + powerConsumption +
+                ", weight=" + weight +
+                ", freezerCapacity=" + freezerCapacity +
+                ", overallCapacity=" + overallCapacity +
+                ", height=" + height +
+                ", width=" + width +
+                '}';
+    }
+}
