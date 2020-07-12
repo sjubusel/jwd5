8e0d619007e974b4bc389ef7fae76c211f666eea
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 21:09:44 +0300

    Add almost complete bean "Laptop", which implements interface "Appliance"

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/impl/Laptop.java
@@ -0,0 +1,114 @@
+package by.epamtc.jwd.busel.assignment05.entity.impl;
+
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+
+public class Laptop implements Appliance {
+    private static final long serialVersionUID = -7974436817656857374L;
+    private Type type;
+    private double batteryCapacity;
+    private String operatingSystem;
+    private int memoryRom;
+    private int systemMemory;
+    private double cpu;
+    private int displayInches;
+
+    public Laptop() {
+    }
+
+    public Laptop(Type type, double batteryCapacity, String operatingSystem,
+            int memoryRom, int systemMemory, double cpu, int displayInches) {
+        this.type = type;
+        this.batteryCapacity = batteryCapacity;
+        this.operatingSystem = operatingSystem;
+        this.memoryRom = memoryRom;
+        this.systemMemory = systemMemory;
+        this.cpu = cpu;
+        this.displayInches = displayInches;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public double getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(double batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public int getMemoryRom() {
+        return memoryRom;
+    }
+
+    public void setMemoryRom(int memoryRom) {
+        this.memoryRom = memoryRom;
+    }
+
+    public int getSystemMemory() {
+        return systemMemory;
+    }
+
+    public void setSystemMemory(int systemMemory) {
+        this.systemMemory = systemMemory;
+    }
+
+    public double getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(double cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getDisplayInches() {
+        return displayInches;
+    }
+
+    public void setDisplayInches(int displayInches) {
+        this.displayInches = displayInches;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return -1;
+    }
+
+    @Override
+    public String receiveTypeName() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "type=" + type +
+                ", batteryCapacity=" + batteryCapacity +
+                ", operatingSystem='" + operatingSystem + '\'' +
+                ", memoryRom=" + memoryRom +
+                ", systemMemory=" + systemMemory +
+                ", cpu=" + cpu +
+                ", displayInches=" + displayInches +
+                '}';
+    }
+}
