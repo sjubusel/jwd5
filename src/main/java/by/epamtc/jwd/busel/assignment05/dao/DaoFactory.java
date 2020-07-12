6e59be94e11812717c4bbbaa9385c1bed94c68af
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 20:00:20 +0300

    Add complete class "DaoFactory" (single-threaded singleton), which is to encapsulate particular realization of "ApplianceDao" from Service-layer

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/DaoFactory.java
@@ -0,0 +1,20 @@
+package by.epamtc.jwd.busel.assignment05.dao;
+
+import by.epamtc.jwd.busel.assignment05.dao.impl.FileApplianceDao;
+
+public class DaoFactory {
+    private static final DaoFactory instance = new DaoFactory();
+
+    private static final ApplianceDao applianceDao = new FileApplianceDao();
+
+    private DaoFactory() {
+    }
+
+    public static DaoFactory getInstance() {
+        return instance;
+    }
+
+    public ApplianceDao getApplianceDao() {
+        return applianceDao;
+    }
+}
