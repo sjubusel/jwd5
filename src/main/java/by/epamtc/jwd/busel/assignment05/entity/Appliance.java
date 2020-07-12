9324eb3413239199b0ae276d8471cc7580b68292
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 21:06:18 +0300

    Realize interface "Appliance"

--- src/main/java/by/epamtc/jwd/busel/assignment05/entity/Appliance.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/Appliance.java
@@ -1,4 +1,23 @@
 package by.epamtc.jwd.busel.assignment05.entity;
 
-public class Appliance {
+import java.io.Serializable;
+
+public interface Appliance extends Serializable {
+    enum Type {
+        OVEN("Oven"), LAPTOP("Laptop"), REFRIGERATOR("Refrigerator"),
+        VACUUM_CLEANER("VacuumCleaner"), TABLET_PC("TabletPC"),
+        SPEAKERS("Speakers");
+
+        private String name;
+
+        Type(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    String receiveTypeName();
 }
