773166636ccab28b52704b3adfac8e5e2a7ed39c
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 19:54:57 +0300

    Add an empty realization of interface "ApplianceDao", which will appeal to a source of data "file"

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/impl/FileApplianceDao.java
@@ -0,0 +1,15 @@
+package by.epamtc.jwd.busel.assignment05.dao.impl;
+
+import by.epamtc.jwd.busel.assignment05.dao.ApplianceDao;
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+import by.epamtc.jwd.busel.assignment05.entity.criteria.Criteria;
+
+import java.util.List;
+
+public class FileApplianceDao implements ApplianceDao {
+    @Override
+    public List<Appliance> find(Criteria criteria) {
+
+        return null;
+    }
+}
