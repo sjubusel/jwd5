28473f84d29617d6339cf894785070a810b0acea
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 21:21:01 +0300

    Add almost complete bean "Oven", which implements interface "Appliance"

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/impl/Oven.java
@@ -0,0 +1,113 @@
+package by.epamtc.jwd.busel.assignment05.entity.impl;
+
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+
+public class Oven implements Appliance {
+    private static final long serialVersionUID = -3346361574124474575L;
+    private Type type;
+    private double powerConsumption;
+    private double weight;
+    private double capacity;
+    private double depth;
+    private double height;
+    private double width;
+
+    public Oven() {
+    }
+
+    public Oven(Type type, double powerConsumption, double weight, double capacity, double depth, double height, double width) {
+        this.type = type;
+        this.powerConsumption = powerConsumption;
+        this.weight = weight;
+        this.capacity = capacity;
+        this.depth = depth;
+        this.height = height;
+        this.width = width;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public double getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(double powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return -1;
+    }
+
+    @Override
+    public String receiveTypeName() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Oven{" +
+                "type=" + type +
+                ", powerConsumption=" + powerConsumption +
+                ", weight=" + weight +
+                ", capacity=" + capacity +
+                ", depth=" + depth +
+                ", height=" + height +
+                ", width=" + width +
+                '}';
+    }
+}
