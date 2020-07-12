8fe8184ef1e08d08fb8f1d22b048197eb5a9e1e8
Siarhei Busiel <sju.busel@gmail.com>
2020-07-12 19:49:59 +0300

    Add interface "ApplianceDao" (plus desired functionality) and create corresponding empty classes "Criteria" and "Appliance"

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/ApplianceDao.java
@@ -0,0 +1,10 @@
+package by.epamtc.jwd.busel.assignment05.dao;
+
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+import by.epamtc.jwd.busel.assignment05.entity.criteria.Criteria;
+
+import java.util.List;
+
+public interface ApplianceDao {
+    List<Appliance> find(Criteria criteria);
+}
---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/Appliance.java
@@ -0,0 +1,4 @@
+package by.epamtc.jwd.busel.assignment05.entity;
+
+public class Appliance {
+}
