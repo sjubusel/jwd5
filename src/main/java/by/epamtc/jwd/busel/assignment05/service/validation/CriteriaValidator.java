0797c7eba22c477279c0d243cbc34bdc19297458
Siarhei Busiel <sju.busel@gmail.com>
2020-07-14 02:12:02 +0300

    Add complete realization of class "CriteriaValidator"

--- src/main/java/by/epamtc/jwd/busel/assignment05/service/validation/CriteriaValidator.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/service/validation/CriteriaValidator.java
@@ -4,9 +4,13 @@ import by.epamtc.jwd.busel.assignment05.entity.Appliance;
 import by.epamtc.jwd.busel.assignment05.entity.criteria.Criteria;
 
 public class CriteriaValidator {
+    private CriteriaValidationProvider provider
+            = new CriteriaValidationProvider();
+
     public boolean isCriteriaValid(Criteria criteria) {
         Appliance.Type type = criteria.getSearchGroupName();
-
-        return false;
+        CriteriaValidation validation;
+        validation = provider.getCommand(type);
+        return validation.isValid(criteria);
     }
 }
