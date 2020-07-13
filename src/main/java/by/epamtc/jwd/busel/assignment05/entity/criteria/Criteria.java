c8dda42b196267aaad56c0b4456643eb040fc422
Siarhei Busiel <sju.busel@gmail.com>
2020-07-13 16:12:50 +0300

    Add 1st realization of class "Criteria"

--- src/main/java/by/epamtc/jwd/busel/assignment05/entity/criteria/Criteria.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/criteria/Criteria.java
@@ -1,4 +1,24 @@
 package by.epamtc.jwd.busel.assignment05.entity.criteria;
 
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Criteria {
+    private Appliance.Type searchGroupName;
+    private Map<String, Object> parameters = new HashMap<>();
+
+    public Criteria(Appliance.Type searchGroupName) {
+        this.searchGroupName = searchGroupName;
+    }
+
+    public Appliance.Type getSearchGroupName() {
+        return searchGroupName;
+    }
+
+    public void add(String parameter, Object value) {
+        parameters.put(parameter, value);
+    }
+
 }
