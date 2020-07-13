e21aabf9bdde0c93db98e8e2c78a6ce22694aad2
Siarhei Busiel <sju.busel@gmail.com>
2020-07-13 23:07:22 +0300

    Add 1st realization of class "LineIntoApplianceParser" (with GoF Command Pattern)

--- src/main/java/by/epamtc/jwd/busel/assignment05/dao/util/LineIntoApplianceParser.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/util/LineIntoApplianceParser.java
@@ -2,9 +2,25 @@ package by.epamtc.jwd.busel.assignment05.dao.util;
 
 import by.epamtc.jwd.busel.assignment05.entity.Appliance;
 
-public class LineIntoApplianceParser {
-    public Appliance parseAppliance(String applianceRecord) {
+import java.util.ArrayList;
+import java.util.List;
 
-        return null;
+public class LineIntoApplianceParser {
+    private static final String PARAMETER_PAIRS_DELIMITER = ", ";
+    private static final String PARAMETER_DELIMITER = "=";
+    private final CommandProvider provider = new CommandProvider();
+
+    public Appliance parseAppliance(String line, Appliance.Type applianceType) {
+        int typePlusDelimiterOffset = applianceType.getName().length() + 3;
+        String[] paramPairs = line.substring(typePlusDelimiterOffset)
+                .split(PARAMETER_PAIRS_DELIMITER);
+
+        List<String> parameters = new ArrayList<>();
+        for (String pair : paramPairs) {
+            int delimiterIndex = pair.indexOf(PARAMETER_DELIMITER);
+            parameters.add(pair.substring(delimiterIndex + 1));
+        }
+        Command executionCommand = provider.getCommand(applianceType);
+        return executionCommand.execute(parameters);
     }
 }
