f7f4e19442dcf8108146c9e87a4e4642144b9ca6
Siarhei Busiel <sju.busel@gmail.com>
2020-07-13 16:57:56 +0300

    Add 1st realization of class "FileApplianceDao"

--- src/main/java/by/epamtc/jwd/busel/assignment05/dao/impl/FileApplianceDao.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/impl/FileApplianceDao.java
@@ -2,15 +2,27 @@ package by.epamtc.jwd.busel.assignment05.dao.impl;
 
 import by.epamtc.jwd.busel.assignment05.dao.ApplianceDao;
 import by.epamtc.jwd.busel.assignment05.dao.exception.DaoException;
+import by.epamtc.jwd.busel.assignment05.dao.util.LineIntoApplianceParser;
+import by.epamtc.jwd.busel.assignment05.dao.util.LineSelector;
 import by.epamtc.jwd.busel.assignment05.entity.Appliance;
 import by.epamtc.jwd.busel.assignment05.entity.criteria.Criteria;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileApplianceDao implements ApplianceDao {
+    LineSelector selector = new LineSelector();
+    LineIntoApplianceParser parser = new LineIntoApplianceParser();
+
     @Override
     public List<Appliance> find(Criteria criteria) throws DaoException {
+        List<String> correctStringRecords = selector.selectAppliances(criteria);
 
-        return null;
+        List<Appliance> appliances = new ArrayList<>();
+        for (String applianceRecord : correctStringRecords) {
+            appliances.add(parser.parseAppliance(applianceRecord));
+        }
+
+        return appliances;
     }
 }
