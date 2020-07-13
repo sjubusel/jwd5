aabf621f65929284cf50fc1a51167b57e56c4536
Siarhei Busiel <sju.busel@gmail.com>
2020-07-13 16:59:05 +0300

    Add empty classes "LineIntoApplianceParser" and "LineSelector"

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/util/LineIntoApplianceParser.java
@@ -0,0 +1,10 @@
+package by.epamtc.jwd.busel.assignment05.dao.util;
+
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+
+public class LineIntoApplianceParser {
+    public Appliance parseAppliance(String applianceRecord) {
+
+        return null;
+    }
+}
---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/dao/util/LineSelector.java
@@ -0,0 +1,13 @@
+package by.epamtc.jwd.busel.assignment05.dao.util;
+
+import by.epamtc.jwd.busel.assignment05.entity.criteria.Criteria;
+
+import java.util.List;
+
+public class LineSelector {
+
+    public List<String> selectAppliances(Criteria criteria) {
+
+        return null;
+    }
+}
