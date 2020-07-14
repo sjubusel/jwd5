e02fc17b5b04b00a24812218d4cc015b4837538a
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 01:59:26 +0300

    Correct final class "SearchCriteria": add verification of corresponding parameter value

--- src/main/java/by/epamtc/jwd/busel/assignment05/entity/criteria/SearchCriteria.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/entity/criteria/SearchCriteria.java
@@ -1,15 +1,74 @@
 package by.epamtc.jwd.busel.assignment05.entity.criteria;
 
+/*
+Enums of this class are used while validation of inputted parameters values.
+Each enum has method "public boolean isAcceptable(Object paramValue)", which is
+to contain acceptable boundaries of parameter value.
+For example:
+If there is a necessity to make a parameter "DISPLAY_INCHS" of enum "Laptop"
+between "10" and "21" inches, a relevant method  "isAcceptable" will be
+overridden in the following way:
+@<code>
+...
+DISPLAY_INCHS(DOUBLE_CLASS) {
+    @Override
+        public boolean isAcceptable(Object paramValue) {
+            Double value = (Double) paramValue;
+            return (value >= 10.0) && (value <= 21.0);
+        }
+};
+...
+</code>
+ */
 public final class SearchCriteria {
     private static final Class<Double> DOUBLE_CLASS = Double.class;
     private static final Class<Integer> INTEGER_CLASS = Integer.class;
     private static final Class<String> STRING_CLASS = String.class;
 
     public enum Oven {
-        POWER_CONSUMPTION(DOUBLE_CLASS), WEIGHT(DOUBLE_CLASS),
-        CAPACITY(DOUBLE_CLASS), DEPTH(DOUBLE_CLASS), HEIGHT(DOUBLE_CLASS),
-        WIDTH(DOUBLE_CLASS);
-        Class<?> classStructure;
+        POWER_CONSUMPTION(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        },
+        WEIGHT(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        },
+        CAPACITY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        },
+        DEPTH(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        },
+        HEIGHT(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        },
+        WIDTH(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        };
+        private Class<?> classStructure;
 
         Oven(Class<?> classStructure) {
             this.classStructure = classStructure;
@@ -18,13 +77,54 @@ public final class SearchCriteria {
         public Class<?> getClassStructure() {
             return classStructure;
         }
+
+        public abstract boolean isAcceptable(Object paramValue);
     }
 
     public enum Laptop {
-        BATTERY_CAPACITY(DOUBLE_CLASS), OS(STRING_CLASS),
-        MEMORY_ROM(DOUBLE_CLASS), SYSTEM_MEMORY(DOUBLE_CLASS),
-        CPU(DOUBLE_CLASS), DISPLAY_INCHS(DOUBLE_CLASS);
-        Class<?> classStructure;
+        BATTERY_CAPACITY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return (value >= 0);
+            }
+        },
+        OS(STRING_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                String value = (String) paramValue;
+                return value.equals("Windows") || value.equals("Linux");
+            }
+        },
+        MEMORY_ROM(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        SYSTEM_MEMORY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        CPU(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        DISPLAY_INCHS(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        };
+        private Class<?> classStructure;
 
         Laptop(Class<?> classStructure) {
             this.classStructure = classStructure;
@@ -33,13 +133,54 @@ public final class SearchCriteria {
         public Class<?> getClassStructure() {
             return classStructure;
         }
+
+        public abstract boolean isAcceptable(Object paramValue);
     }
 
     public enum Refrigerator {
-        POWER_CONSUMPTION(DOUBLE_CLASS), WEIGHT(DOUBLE_CLASS),
-        FREEZER_CAPACITY(DOUBLE_CLASS), OVERALL_CAPACITY(DOUBLE_CLASS),
-        HEIGHT(DOUBLE_CLASS), WIDTH(DOUBLE_CLASS);
-        Class<?> classStructure;
+        POWER_CONSUMPTION(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        WEIGHT(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        FREEZER_CAPACITY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        OVERALL_CAPACITY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        HEIGHT(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        WIDTH(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        };
+        private Class<?> classStructure;
 
         Refrigerator(Class<?> classStructure) {
             this.classStructure = classStructure;
@@ -48,13 +189,51 @@ public final class SearchCriteria {
         public Class<?> getClassStructure() {
             return classStructure;
         }
+
+        public abstract boolean isAcceptable(Object paramValue);
     }
 
     public enum VacuumCleaner {
-        POWER_CONSUMPTION(DOUBLE_CLASS), FILTER_TYPE(STRING_CLASS),
-        BAG_TYPE(STRING_CLASS), WAND_TYPE(STRING_CLASS),
-        MOTOR_SPEED_REGULATION(DOUBLE_CLASS), CLEANING_WIDTH(DOUBLE_CLASS);
-        Class<?> classStructure;
+        POWER_CONSUMPTION(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        FILTER_TYPE(STRING_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                return true;
+            }
+        },
+        BAG_TYPE(STRING_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                return true;
+            }
+        },
+        WAND_TYPE(STRING_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                return true;
+            }
+        },
+        MOTOR_SPEED_REGULATION(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        CLEANING_WIDTH(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        };
+        private Class<?> classStructure;
 
         VacuumCleaner(Class<?> classStructure) {
             this.classStructure = classStructure;
@@ -63,13 +242,46 @@ public final class SearchCriteria {
         public Class<?> getClassStructure() {
             return classStructure;
         }
+
+        public abstract boolean isAcceptable(Object paramValue);
     }
 
     public enum TabletPc {
-        BATTERY_CAPACITY(DOUBLE_CLASS), DISPLAY_INCHES(DOUBLE_CLASS),
-        MEMORY_ROM(DOUBLE_CLASS), FLASH_MEMORY_CAPACITY(DOUBLE_CLASS),
-        COLOR(STRING_CLASS);
-        Class<?> classStructure;
+        BATTERY_CAPACITY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        DISPLAY_INCHES(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        MEMORY_ROM(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        FLASH_MEMORY_CAPACITY(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        COLOR(STRING_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                return true;
+            }
+        };
+        private Class<?> classStructure;
 
         TabletPc(Class<?> classStructure) {
             this.classStructure = classStructure;
@@ -78,12 +290,39 @@ public final class SearchCriteria {
         public Class<?> getClassStructure() {
             return classStructure;
         }
+
+        public abstract boolean isAcceptable(Object paramValue);
     }
 
     public enum Speakers {
-        POWER_CONSUMPTION(DOUBLE_CLASS), NUMBER_OF_SPEAKERS(INTEGER_CLASS),
-        FREQUENCY_RANGE(STRING_CLASS), CORD_LENGTH(DOUBLE_CLASS);
-        Class<?> classStructure;
+        POWER_CONSUMPTION(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        },
+        NUMBER_OF_SPEAKERS(INTEGER_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Integer value = (Integer) paramValue;
+                return value >= 0;
+            }
+        },
+        FREQUENCY_RANGE(STRING_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                return true;
+            }
+        },
+        CORD_LENGTH(DOUBLE_CLASS) {
+            @Override
+            public boolean isAcceptable(Object paramValue) {
+                Double value = (Double) paramValue;
+                return value >= 0;
+            }
+        };
+        private Class<?> classStructure;
 
         Speakers(Class<?> classStructure) {
             this.classStructure = classStructure;
@@ -92,6 +331,8 @@ public final class SearchCriteria {
         public Class<?> getClassStructure() {
             return classStructure;
         }
+
+        public abstract boolean isAcceptable(Object paramValue);
     }
 
     private SearchCriteria() {
