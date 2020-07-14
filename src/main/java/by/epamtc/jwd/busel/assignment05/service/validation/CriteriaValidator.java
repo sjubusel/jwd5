40dcf562b5c31b0112f38c7e047c039107121417
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 02:06:44 +0300

    Add "throws" to a method "public boolean isCriteriaValid(Criteria criteria)" of class "CriteriaValidator"

--- src/main/java/by/epamtc/jwd/busel/assignment05/service/validation/CriteriaValidator.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/service/validation/CriteriaValidator.java
@@ -2,12 +2,13 @@ package by.epamtc.jwd.busel.assignment05.service.validation;
 
 import by.epamtc.jwd.busel.assignment05.entity.Appliance;
 import by.epamtc.jwd.busel.assignment05.entity.criteria.Criteria;
+import by.epamtc.jwd.busel.assignment05.service.exception.ServiceException;
 
 public class CriteriaValidator {
     private CriteriaValidationProvider provider
             = new CriteriaValidationProvider();
 
-    public boolean isCriteriaValid(Criteria criteria) {
+    public boolean isCriteriaValid(Criteria criteria) throws ServiceException {
         Appliance.Type type = criteria.getSearchGroupName();
         CriteriaValidation validation = provider.getValidation(type);
         return validation.isValid(criteria);
