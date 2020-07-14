b7cfed61226a27740651e489390d7b41045a5fc2
Siarhei Busiel <sju.busel@gmail.com>
2020-07-14 06:14:19 +0300

    Edit interface "AppliancePrinter"

--- src/main/java/by/epamtc/jwd/busel/assignment05/main/printer/AppliancePrinter.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/main/printer/AppliancePrinter.java
@@ -3,5 +3,10 @@ package by.epamtc.jwd.busel.assignment05.main.printer;
 import by.epamtc.jwd.busel.assignment05.entity.Appliance;
 
 public interface AppliancePrinter {
+    String TYPE_DELIMITER = " : ";
+    String PARAMETER_DELIMITER = ", ";
+    String KEY_VALUE_DELIMITER = "=";
+    String RECORD_ENDING = ";";
+
     void print(Appliance appliance);
 }
