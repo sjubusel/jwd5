a390ed0f8f5789547d236ebbf876483b849819fb
Siarhei Busiel <sju.busel@gmail.com>
2020-07-14 06:19:36 +0300

    Correct class "Main"

--- src/main/java/by/epamtc/jwd/busel/assignment05/main/Main.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/main/Main.java
@@ -17,13 +17,15 @@ public class Main {
         ServiceFactory factory = ServiceFactory.getInstance();
         ApplianceService service = factory.getApplianceService();
 
-        Criteria criteriaOven = new Criteria(Appliance.Type.TABLET_PC);
-        criteriaOven.add(TabletPc.DISPLAY_INCHES.toString(), 15.0);
-        criteriaOven.add(TabletPc.BATTERY_CAPACITY.toString(), 4.0);
-        criteriaOven.add(TabletPc.COLOR.toString(), "red");
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteria1 = new Criteria(Appliance.Type.TABLET_PC);
+        criteria1.add(TabletPc.DISPLAY_INCHES.toString(), 15.0);
+        criteria1.add(TabletPc.BATTERY_CAPACITY.toString(), 4.0);
+        criteria1.add(TabletPc.COLOR.toString(), "red");
 
         try {
-            appliances = service.find(criteriaOven);
+            appliances = service.find(criteria1);
         } catch (ServiceException e) {
             System.err.println(e.toString());
             System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
@@ -31,6 +33,22 @@ public class Main {
             return;
         }
 
-        PrintApplianceInfo.print(appliances, criteriaOven);
+        PrintApplianceInfo.print(appliances, criteria1);
+
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteria2 = new Criteria(Appliance.Type.OVEN);
+        criteria2.add(Oven.WIDTH.toString(), 70.0);
+
+        try {
+            appliances = service.find(criteria2);
+        } catch (ServiceException e) {
+            System.err.println(e.toString());
+            System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
+                    "sergey.busel@tut.by");
+            return;
+        }
+
+        PrintApplianceInfo.print(appliances, criteria2);
     }
 }
