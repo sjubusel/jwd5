195d9fd54b5cace65be73194a74951c81e33b4fb
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 02:43:02 +0300

    Correct class "Main" as if it is a test class

--- src/main/java/by/epamtc/jwd/busel/assignment05/main/Main.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/main/Main.java
@@ -19,13 +19,11 @@ public class Main {
 
         ////////////////////////////////////////////////////////////////////////
 
-        Criteria criteria1 = new Criteria(Appliance.Type.TABLET_PC);
-        criteria1.add(TabletPc.DISPLAY_INCHES.toString(), 15.0);
-        criteria1.add(TabletPc.BATTERY_CAPACITY.toString(), 4.0);
-        criteria1.add(TabletPc.COLOR.toString(), "red");
+        Criteria criteriaOven = new Criteria(Appliance.Type.OVEN);
+        criteriaOven.add(Oven.WIDTH.toString(), 70.0);
 
         try {
-            appliances = service.find(criteria1);
+            appliances = service.find(criteriaOven);
         } catch (ServiceException e) {
             System.err.println(e.toString());
             System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
@@ -33,15 +31,16 @@ public class Main {
             return;
         }
 
-        PrintApplianceInfo.print(appliances, criteria1);
+        PrintApplianceInfo.print(appliances, criteriaOven);
+        System.out.println();
 
         ////////////////////////////////////////////////////////////////////////
 
-        Criteria criteria2 = new Criteria(Appliance.Type.OVEN);
-        criteria2.add(Oven.WIDTH.toString(), 70.0);
+        Criteria criteriaLaptop = new Criteria(Appliance.Type.LAPTOP);
+        criteriaLaptop.add(Laptop.BATTERY_CAPACITY.toString(), 1.0);
 
         try {
-            appliances = service.find(criteria2);
+            appliances = service.find(criteriaLaptop);
         } catch (ServiceException e) {
             System.err.println(e.toString());
             System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
@@ -49,6 +48,97 @@ public class Main {
             return;
         }
 
-        PrintApplianceInfo.print(appliances, criteria2);
+        PrintApplianceInfo.print(appliances, criteriaLaptop);
+        System.out.println();
+
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteriaRefrigerator = new Criteria(Appliance.Type.REFRIGERATOR);
+        criteriaRefrigerator.add(Refrigerator.WEIGHT.toString(), 20.0);
+
+        try {
+            appliances = service.find(criteriaRefrigerator);
+        } catch (ServiceException e) {
+            System.err.println(e.toString());
+            System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
+                    "sergey.busel@tut.by");
+            return;
+        }
+
+        PrintApplianceInfo.print(appliances, criteriaRefrigerator);
+        System.out.println();
+
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteriaVacuumCleaner = new Criteria(Appliance.Type.VACUUM_CLEANER);
+        criteriaVacuumCleaner.add(VacuumCleaner.FILTER_TYPE.toString(), "B");
+
+        try {
+            appliances = service.find(criteriaVacuumCleaner);
+        } catch (ServiceException e) {
+            System.err.println(e.toString());
+            System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
+                    "sergey.busel@tut.by");
+            return;
+        }
+
+        PrintApplianceInfo.print(appliances, criteriaVacuumCleaner);
+        System.out.println();
+
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteriaTabletPc = new Criteria(Appliance.Type.TABLET_PC);
+        criteriaTabletPc.add(TabletPc.DISPLAY_INCHES.toString(), 15.0);
+        criteriaTabletPc.add(TabletPc.BATTERY_CAPACITY.toString(), 4.0);
+        criteriaTabletPc.add(TabletPc.COLOR.toString(), "red");
+
+        try {
+            appliances = service.find(criteriaTabletPc);
+        } catch (ServiceException e) {
+            System.err.println(e.toString());
+            System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
+                    "sergey.busel@tut.by");
+            return;
+        }
+
+        PrintApplianceInfo.print(appliances, criteriaTabletPc);
+        System.out.println();
+
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteriaSpeakers = new Criteria(Appliance.Type.SPEAKERS);
+        criteriaSpeakers.add(Speakers.FREQUENCY_RANGE.toString(), "2-3.5");
+        criteriaSpeakers.add(Speakers.NUMBER_OF_SPEAKERS.toString(), 4);
+
+        try {
+            appliances = service.find(criteriaSpeakers);
+        } catch (ServiceException e) {
+            System.err.println(e.toString());
+            System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
+                    "sergey.busel@tut.by");
+            return;
+        }
+
+        PrintApplianceInfo.print(appliances, criteriaSpeakers);
+        System.out.println();
+
+        ////////////////////////////////////////////////////////////////////////
+
+        Criteria criteriaNotFound = new Criteria(Appliance.Type.SPEAKERS);
+        criteriaNotFound.add(Speakers.FREQUENCY_RANGE.toString(), "1");
+        criteriaNotFound.add(Speakers.NUMBER_OF_SPEAKERS.toString(), 4);
+
+        try {
+            appliances = service.find(criteriaNotFound);
+        } catch (ServiceException e) {
+            System.err.println(e.toString());
+            System.out.println("SOMETHING WENT WRONG, PLEASE CONTACT US.\n" +
+                    "sergey.busel@tut.by");
+            return;
+        }
+
+        PrintApplianceInfo.print(appliances, criteriaNotFound);
+        System.out.println();
+
     }
 }
