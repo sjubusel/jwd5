353db6f09e1df737a008cd83101e1d390c48d2ea
Siarhei Busiel <sju.busel@gmail.com>
2020-07-14 05:15:37 +0300

    Add interface AppliancePrinter

---
+++ src/main/java/by/epamtc/jwd/busel/assignment05/main/printer/AppliancePrinter.java
@@ -0,0 +1,7 @@
+package by.epamtc.jwd.busel.assignment05.main.printer;
+
+import by.epamtc.jwd.busel.assignment05.entity.Appliance;
+
+public interface AppliancePrinter {
+    void print(Appliance appliance);
+}
