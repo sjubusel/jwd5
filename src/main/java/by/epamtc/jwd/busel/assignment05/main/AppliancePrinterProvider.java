be819ca0035e04dee80956e881a7f39dedd6965d
Siarhei Busiel <sju.busel@gmail.com>
2020-07-14 06:13:49 +0300

    Add class "AppliancePrinterProvider"

--- src/main/java/by/epamtc/jwd/busel/assignment05/main/AppliancePrinterProvider.java
+++ src/main/java/by/epamtc/jwd/busel/assignment05/main/AppliancePrinterProvider.java
@@ -1,27 +1,23 @@
 package by.epamtc.jwd.busel.assignment05.main;
 
-import by.epamtc.jwd.busel.assignment05.entity.Appliance;
 import by.epamtc.jwd.busel.assignment05.main.printer.AppliancePrinter;
-import by.epamtc.jwd.busel.assignment05.service.validation.CriteriaValidation;
-import by.epamtc.jwd.busel.assignment05.service.validation.impl.*;
+import by.epamtc.jwd.busel.assignment05.main.printer.impl.*;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static by.epamtc.jwd.busel.assignment05.entity.Appliance.Type.*;
 
 public class AppliancePrinterProvider {
     private final Map<String, AppliancePrinter> repository = new HashMap<>();
 
     AppliancePrinterProvider() {
-//        repository.put(Appliance.Type.OVEN, new OvenCriteriaValidation());
-//        repository.put(Appliance.Type.LAPTOP, new LaptopCriteriaValidation());
-//        repository.put(Appliance.Type.REFRIGERATOR,
-//                new RefrigeratorCriteriaValidation());
-//        repository.put(Appliance.Type.VACUUM_CLEANER,
-//                new VacuumCleanerCriteriaValidation());
-//        repository.put(Appliance.Type.TABLET_PC,
-//                new TabletPcCriteriaValidation());
-//        repository.put(Appliance.Type.SPEAKERS,
-//                new SpeakersCriteriaValidation());
+        repository.put(OVEN.getName(), new OvenPrinter());
+        repository.put(LAPTOP.getName(), new LaptopPrinter());
+        repository.put(REFRIGERATOR.getName(), new RefrigeratorPrinter());
+        repository.put(VACUUM_CLEANER.getName(), new VacuumCleanerPrinter());
+        repository.put(TABLET_PC.getName(), new TabletPcPrinter());
+        repository.put(SPEAKERS.getName(), new SpeakersPrinter());
     }
 
     public AppliancePrinter getAppliancePrinter(String typeName) {
